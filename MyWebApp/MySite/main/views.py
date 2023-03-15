1faf1e45d306a96812bfec0a3297c3866bed5053
shmelof <shmelof1@gmail.com>
2023-03-15 11:15:02 +0500

    for-sima-land
    for-sima-land

---
+++ MyWebApp/MySite/main/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+
+def index(request):
+    data = {
+        'title': 'Home page'
+    }
+    return render(request, 'main/index.html', data)
+
+
+def about(request):
+    return render(request, 'main/about.html')
+
+
+def contacts(request):
+    return render(request, 'main/contacts.html')
